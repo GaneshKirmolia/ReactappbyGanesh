ac1bebefb5ad403800cc8bf4a6ed4ddfa8fceb89
Ganesh Kirmolia <74208899+GaneshDa@users.noreply.github.com>
2025-07-18 01:39:44 +0530

    changes in pankaj branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Updating Deplyemnt version 1.0.1
+        Checking thrigger from my end
         </p>
         <a
           className="App-link"
