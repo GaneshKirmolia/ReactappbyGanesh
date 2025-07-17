8a0f88a0442045a0ec8fea0ec06e53d1ece44b09
Ganesh Singh Kirmolia <74208899+GaneshKirmolia@users.noreply.github.com>
2025-07-18 03:03:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Checking thrigger from my end
+        checking cloudbuild trigger working or cloud run deply or not
         </p>
         <a
           className="App-link"
